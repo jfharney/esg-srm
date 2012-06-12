74e65e49d2faa3e5a41fbe913ba486b5856c9974
Harney <8xo@lap00091497.ornl.gov>
2012-06-12 14:40:58 -0400

    Extras 2

---
+++ src/java/main/org/esgf/srm/SRMRequestController.java
@@ -0,0 +1,40 @@
+package org.esgf.srm;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import dw.spring3.rest.bean.Employee;
+
+@Controller
+public class SRMRequestController {
+
+	
+	@RequestMapping(method=RequestMethod.GET, value="/srmrequest")
+    public @ResponseBody String getSRMRequest(HttpServletRequest request) {
+		System.out.println("In getSRMRequest");
+		String response = "<srm_url>srm</srm_url>";
+	
+		return response;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/srmrequest")
+	//public ModelAndView addEmployee(@RequestBody String body) {
+	public @ResponseBody void addSRMRequest(@RequestBody String body, HttpServletRequest request) {
+		System.out.println("In addSRMRequest");
+		
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/srmrequest")
+	public @ResponseBody void removeEmployee(HttpServletRequest request) {
+		System.out.println("In removeSRMRequest");
+		
+	}
+	
+	
+}
